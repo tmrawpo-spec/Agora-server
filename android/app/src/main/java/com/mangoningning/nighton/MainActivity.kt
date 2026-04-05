5ac7bc70fac46d68e7592bd435246ada4f1dd3dd
tmrawpo-spec <tmrawpo@gmail.com>
2026-04-05 18:56:05 -0400

    change call notification channel

--- android/app/src/main/java/com/mangoningning/nighton/MainActivity.kt
+++ android/app/src/main/java/com/mangoningning/nighton/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nighton
+package com.mangoningning.nighton
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/mangoningning/nighton/MainApplication.kt
+++ android/app/src/main/java/com/mangoningning/nighton/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.nighton
+package com.mangoningning.nighton
 
 import android.app.Application
 import android.content.res.Configuration
